2d792eb9912d71eff6ba06f04fe329b1984c0c36
MikaO13 <mika.disneypins@gmail.com>
2021-11-17 20:46:29 -0800

    sidekick

--- src/main/java/frc/team3128/subsystems/Constants.java
+++ src/main/java/frc/team3128/subsystems/Constants.java
@@ -1,5 +1,22 @@
 package frc.team3128.subsystems;
 
 public class Constants {
-    
+    public static class SidekickConstants {
+        public static final float SIDEKICK_PID_kP = 0;
+        public static final float SIDEKICK_PID_kI = 0;
+        public static final float SIDEKICK_PID_kD = 0;
+        public static final int SIDEKICK_RPM_THRESHOLD_PERCENT = 0;
+        public static final int SIDEKICK_ID = 0;
+        public static final int CAN_TIMEOUT = 0;
+        public static final double SIDEKICK_kS = 0;
+        public static final double SIDEKICK_kV = 0;
+    }
+
+    public static class ConversionConstants {
+        public static final double SIDEKICK_ENCODER_TO_RPM = 10*60/MechanismConstants.SIDEKICK_UNITS_PER_ROTATION;
+    }
+
+    public static class MechanismConstants {
+        public static final double SIDEKICK_UNITS_PER_ROTATION = 4096;
+    }
 }
