3eab9b30b994df132a01e13c4d03fdbe84b19b17
kayoonk <kayoonk02@gmail.com>
2021-11-19 16:14:06 -0800

    Create NAR_CANSparkMax.java

---
+++ src/main/java/frc/team3128/hardware/NAR_CANSparkMax.java
@@ -0,0 +1,42 @@
+package frc.team3128.hardware;
+
+import com.revrobotics.*;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+
+
+public class NAR_CANSparkMax extends CANSparkMax {
+
+	private double prevValue = 0;
+	private ControlType prevControlMode = ControlType.kVoltage;
+
+	/**
+	 * 
+	 * @param deviceNumber device id
+	 * @param type         0 for brushed motor, 1 for brushless motor
+	 */
+
+	public NAR_CANSparkMax(int deviceNumber, MotorType type) {
+		super(deviceNumber, type);
+		// enableVoltageCompensation(true);
+		// configVoltageCompSaturation(12, 10);
+	}
+
+	@Override
+	public void set(double outputValue) {
+
+		if (outputValue != prevValue) {
+			super.set(outputValue);
+			prevValue = outputValue;
+		}
+
+	}
+
+	public double getSetpoint() {
+		return prevValue;
+	}
+}
+
+
+}
