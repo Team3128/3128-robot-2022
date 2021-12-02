94cc52fcd162b780d627a586d6cf007ae4e9761d
jennymar <56415672+jennymar@users.noreply.github.com>
2021-12-01 20:10:02 -0800

    intake and shoot work

--- src/main/java/frc/team3128/RobotContainer.java
+++ src/main/java/frc/team3128/RobotContainer.java
@@ -19,6 +19,7 @@ public class RobotContainer {
 
     private NAR_Drivetrain m_drive;
     private Shooter m_shooter;
+    private Sidekick m_sidekick;
     private Hopper m_hopper;
     private Intake m_intake;
 
@@ -30,20 +31,22 @@ public class RobotContainer {
 
     public RobotContainer() {
 
-        m_drive = new NAR_Drivetrain();
-        m_shooter = new Shooter();
-        m_hopper = new Hopper();
-        m_intake = new Intake();
+        m_drive = NAR_Drivetrain.getInstance();
+        m_shooter = Shooter.getInstance();
+        m_sidekick = Sidekick.getInstance();
+        m_hopper = Hopper.getInstance();
+        m_intake = Intake.getInstance();
         m_shooter.enable();
+        m_sidekick.enable();
 
         m_leftStick = new NAR_Joystick(0);
         m_rightStick = new NAR_Joystick(1);
 
         //Registers subsystems so that periodic methods run
-        m_commandScheduler.registerSubsystem(m_drive, m_shooter, m_hopper, m_intake);
+        m_commandScheduler.registerSubsystem(m_drive, m_shooter, m_sidekick, m_hopper, m_intake);
 
         m_commandScheduler.setDefaultCommand(m_drive, new ArcadeDrive(m_drive, m_rightStick::getY, m_rightStick::getX));
-        m_commandScheduler.setDefaultCommand(m_hopper, new HopperDefault(m_hopper, m_shooter::atSetpoint));
+        m_commandScheduler.setDefaultCommand(m_hopper, new HopperDefault(m_hopper, m_shooter::atSetpoint, m_sidekick::isReady));
 
         configureButtonBindings();
     }   
@@ -55,7 +58,7 @@ public class RobotContainer {
                                 .whenReleased(new RunCommand(m_intake::stopIntake, m_intake));
 
         // right button 2: shoot
-        m_rightStick.getButton(2).whenPressed(new Shoot(m_shooter, ShooterState.MID_RANGE))
+        m_rightStick.getButton(2).whenPressed(new Shoot(m_shooter, m_sidekick, ShooterState.MID_RANGE))
                                 .whenReleased(new RunCommand(m_shooter::stopShoot, m_shooter));
 
         // right button 9: move arm down
