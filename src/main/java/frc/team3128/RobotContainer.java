3be1ed9757f2c60bf89ec61d78681fc42d0fec5a
jude-lif <43428384+jude-lif@users.noreply.github.com>
2022-01-07 15:05:52 -0800

    main pre-kickoff commit

--- src/main/java/frc/team3128/RobotContainer.java
+++ src/main/java/frc/team3128/RobotContainer.java
@@ -1,16 +1,14 @@
 package frc.team3128;
 
 import frc.team3128.subsystems.*;
-import frc.team3128.subsystems.Shooter.ShooterState;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.team3128.commands.*;
 import frc.team3128.common.hardware.input.NAR_Joystick;
 import frc.team3128.common.limelight.LEDMode;
 import frc.team3128.common.limelight.Limelight;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -22,13 +20,8 @@ import frc.team3128.common.limelight.Limelight;
 public class RobotContainer {
 
     private NAR_Drivetrain m_drive;
-    private Shooter m_shooter;
-    private Sidekick m_sidekick;
-    private Hopper m_hopper;
-    private Intake m_intake;
-    private Climber m_climber;
 
-    private Limelight shooterLimelight = new Limelight("limelight-sog", -26.0, 0, 0, 30);
+    private Limelight limelight = new Limelight("limelight-sog", -26.0, 0, 0, 30);
 
     private NAR_Joystick m_leftStick;
     private NAR_Joystick m_rightStick;
@@ -39,48 +32,21 @@ public class RobotContainer {
     public RobotContainer() {
 
         m_drive = NAR_Drivetrain.getInstance();
-        m_shooter = Shooter.getInstance();
-        m_sidekick = Sidekick.getInstance();
-        m_hopper = Hopper.getInstance();
-        m_intake = Intake.getInstance();
-        m_climber = Climber.getInstance();
 
         //Enable all PIDSubsystems so that useOutput runs
-        m_shooter.enable();
-        m_sidekick.enable();
 
         m_leftStick = new NAR_Joystick(0);
         m_rightStick = new NAR_Joystick(1);
 
-        shooterLimelight.setLEDMode(LEDMode.ON);
+        limelight.setLEDMode(LEDMode.ON);
 
         m_commandScheduler.setDefaultCommand(m_drive, new ArcadeDrive(m_drive, m_rightStick::getY, m_rightStick::getTwist, m_rightStick::getThrottle));
-        m_commandScheduler.setDefaultCommand(m_hopper, new HopperDefault(m_hopper, m_shooter::atSetpoint));
 
         configureButtonBindings();
         dashboardInit();
     }   
 
     private void configureButtonBindings() {
-
-
-        // right button trigger: intake
-        m_rightStick.getButton(1).whenActive(new RunCommand(m_intake::runIntake, m_intake));
-
-        // right button 2: shoot
-        m_rightStick.getButton(2).whenActive(new ParallelCommandGroup(new Shoot(m_shooter, m_sidekick, ShooterState.MID_RANGE), new CmdAlign(m_drive, shooterLimelight)));
-
-        // right button 9: move arm down
-        m_rightStick.getButton(9).whenActive(new RunCommand(m_intake::moveArmDown, m_intake));
-        
-        // right button 10: move arm up
-        m_rightStick.getButton(10).whenActive(new RunCommand(m_intake::moveArmUp, m_intake));
-
-        // left button 7: move climber up
-        m_leftStick.getButton(7).whenActive(new RunCommand(m_climber::moveClimberUp, m_climber));
-
-        // left button 8: move climber down
-        m_leftStick.getButton(8).whenActive(new RunCommand(m_climber::moveClimberDown, m_climber));
         
     }
 
@@ -88,11 +54,6 @@ public class RobotContainer {
         SmartDashboard.putData(CommandScheduler.getInstance());
         
         SmartDashboard.putData(m_drive);
-        SmartDashboard.putData(m_shooter);
-        SmartDashboard.putData(m_sidekick);
-        SmartDashboard.putData(m_hopper);
-        SmartDashboard.putData(m_intake);
-        SmartDashboard.putData(m_climber);
 
     }
 
--- src/main/java/frc/team3128/subsystems/Constants.java
+++ src/main/java/frc/team3128/subsystems/Constants.java
@@ -10,62 +10,9 @@ import edu.wpi.first.wpiutil.math.numbers.N2;
 
 public class Constants {
 
-    public static class ShooterConstants {
-
-        public static final double SHOOTER_PID_kP = 1.24e-7;
-        public static final double SHOOTER_PID_kI = 0;
-        public static final double SHOOTER_PID_kD = 0;
-        public static final double SHOOTER_PID_kF = 0;
-
-        public static final int LEFT_SHOOTER_ID = 8; //Left Shooter Motor
-        public static final int RIGHT_SHOOTER_ID = 13; //Right Shooter Motor
-
-        public static final int PLATEAU_COUNT = 25; //Number of checks at correct RPM to shoot
-        public static final double THRESHOLD_PERCENT = 0.05; //Maximum Percent Error in RPM to still shoot
-        public static final double RPM_THRESHOLD_PERCENT = 0.05;
-        public static final double RPM_THRESHOLD_PERCENT_MAX = 0.1;
-        public static final double TIME_TO_MAX_THRESHOLD = 5;
-
-        public static final double SHOOTER_KS = 0.711; //Static gain in PID Feed Forward
-        public static final double SHOOTER_KV = 0.00163; //Velocity gain in PID Feed Forward
-        public static final double SHOOTER_KA = 0.0349; //Acceleration gain PID Feed Forward
-
-        // There are other better ways of doing this, need to use DCU 
-        public static final LinearSystem<N1, N1, N1> SHOOTER_CHAR = 
-        LinearSystemId.identifyVelocitySystem(
-            SHOOTER_KV, 
-            SHOOTER_KA
-        );
-        public static final DCMotor SHOOTER_GEARBOX = DCMotor.getFalcon500(2);
-        public static final double SHOOTER_GEARING = 1.5;
-        public static final double SHOOTER_RADIUS_METERS = 0.0508; //not correct
-  
-       
-    }
-
-    public static class SidekickConstants {
-
-        public static final int SIDEKICK_ID = 42;
-
-        public static final float SIDEKICK_PID_kP = 0;
-        public static final float SIDEKICK_PID_kI = 0;
-        public static final float SIDEKICK_PID_kD = 0;
-
-        public static final int SIDEKICK_RPM_THRESHOLD_PERCENT = 0;
-        public static final double SIDEKICK_PLATEAU_THRESHOLD = 5;
-
-        public static final int CAN_TIMEOUT = 0;
-        public static final double SIDEKICK_kS = 0;
-        public static final double SIDEKICK_kV = 0;
-
-        public static final double SIDEKICK_UNITS_PER_ROTATION = 4096;
-
-    }
-
     public static class ConversionConstants {
         public static final double FALCON_ENCODER_RESOLUTION = 2048;
         public static final double ENCODER_TO_RPM = 10*60/FALCON_ENCODER_RESOLUTION; // (sensor units per 100 ms to rpm)
-        public static final double SIDEKICK_ENCODER_TO_RPM = 10*60/SidekickConstants.SIDEKICK_UNITS_PER_ROTATION;
 
     }
 
@@ -94,48 +41,6 @@ public class Constants {
         public static final double ENCODER_RESOLUTION_PER_ROTATION = 2048;
         public static final double ENCODER_DISTANCE_PER_MARK = WHEEL_RADIUS_METERS * 2 / ENCODER_RESOLUTION_PER_ROTATION;
 
-    }
-
-    public static class IntakeConstants {
-        public static final int ARM_MOTOR_ID = 7;
-        public static final int BRUSH_MOTOR_1_ID = 5;
-        public static final int BRUSH_MOTOR_2_ID = 33;
-        public static final int INTAKE_MOTOR_ID = 4;
-
-        public static final int TOP_LIMIT_SWITCH_ID = 2;
-        public static final int BOTTOM_LIMIT_SWITCH_ID = 3;
-
-        public static final double INTAKE_MOTOR_POWER = 0.6;
-        public static final double BRUSH_MOTOR_POWER = 0.3;
-        public static final double ARM_MOTOR_POWER = 0.42;
-
-        public static final double ARM_MOTOR_POWER_AUTO = 0.07;
-        public static final NeutralMode ARM_NEUTRAL_MODE = NeutralMode.Brake;
-    }
-
-    public static class HopperConstants {
-
-        public static final int HOPPER_MOTOR_1_ID = 6;
-        public static final int HOPPER_MOTOR_2_ID = 19;
-
-        public static final int BOTTOM_SENSOR_ID = 9;
-        public static final int TOP_SENSOR_ID = 8;
-
-        public static final double HOPPER_MOTOR_1_POWER = -0.5;
-        public static final double HOPPER_MOTOR_2_POWER = -0.3;
-        
-
-    }
-  
-    public static class ClimberConstants {
-
-        public static final int CLIMBER_MOTOR_1_ID = 10;
-        public static final int CLIMBER_MOTOR_2_ID = 11;
-
-        public static final double CLIMBER_UP_POWER = 1;
-        public static final double CLIMBER_DOWN_POWER = -1;
-    
-        public static final NeutralMode CLIMBER_NEUTRAL_MODE = NeutralMode.Brake;
     }
 
     public static class VisionContants {
