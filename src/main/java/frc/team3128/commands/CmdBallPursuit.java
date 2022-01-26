5acf569d0be1eaf489f314e4e9d253d045a2e26c
Mika Okamoto <mika.disneypins@gmail.com>
2022-01-25 19:15:39 -0800

    Update CmdBallPursuit.java

--- src/main/java/frc/team3128/commands/CmdBallPursuit.java
+++ src/main/java/frc/team3128/commands/CmdBallPursuit.java
@@ -82,6 +82,7 @@ public class CmdBallPursuit extends CommandBase {
                 if (!ballLimelight.hasValidTarget()) {
                     Log.info("CmdBallPursuit", "No valid target anymore.");
                     aimState = BallPursuitState.SEARCHING;
+                    targetCount = 0;
                 } else {
                     currentHorizontalOffset = ballLimelight.getValue(LimelightKey.HORIZONTAL_OFFSET, 5);
 
@@ -123,6 +124,7 @@ public class CmdBallPursuit extends CommandBase {
                 if (ballLimelight.hasValidTarget()) {
                     Log.info("CmdBallPursuit", "Target found - Switching to SEARCHING");
                     aimState = BallPursuitState.SEARCHING;
+                    targetCount = 0;
                 }
                     
                 break;
