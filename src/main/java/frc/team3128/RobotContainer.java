82b51085f1b02c369a6791ac14c5aa476f7a35f5
danielwang546 <42781202+danielwang546@users.noreply.github.com>
2022-02-05 10:59:50 -0800

    cleanup imports in robotcontainer

--- src/main/java/frc/team3128/RobotContainer.java
+++ src/main/java/frc/team3128/RobotContainer.java
@@ -15,19 +15,11 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
-
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.team3128.commands.*;
 import frc.team3128.common.hardware.input.NAR_Joystick;
 import frc.team3128.common.hardware.limelight.Limelight;
 import frc.team3128.common.narwhaldashboard.NarwhalDashboard;
 import frc.team3128.common.utility.Log;
-import frc.team3128.commands.*;
 import frc.team3128.subsystems.*;
 
 /**
