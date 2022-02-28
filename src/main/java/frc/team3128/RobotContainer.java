3bd57be936e4114345237623388d0756cd9f34b7
danielwang546 <42781202+danielwang546@users.noreply.github.com>
2022-02-27 16:28:12 -0800

    Change climb cancel logic

--- src/main/java/frc/team3128/RobotContainer.java
+++ src/main/java/frc/team3128/RobotContainer.java
@@ -175,7 +175,7 @@ public class RobotContainer {
 
         m_rightStick.getButton(8).whenHeld(extendIntakeAndReverse);
 
-        m_rightStick.getButton(10).whenHeld(new InstantCommand(m_climber::bothStop, m_climber));
+        m_rightStick.getButton(10).whenPressed(new InstantCommand(m_climber::bothStop, m_climber));
 
         m_rightStick.getButton(13).whenHeld(new SequentialCommandGroup(
             new CmdRetractHopper(m_hopper),
