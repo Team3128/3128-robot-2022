0397649fbdf9d9846e19fe8ea412686a5e14a57f
Team 3128: Controls <35188310+ControlsNarwhal@users.noreply.github.com>
2022-03-09 20:00:04 -0800

    Added 3 ball path using in place turns

--- src/main/java/frc/team3128/commands/CmdInPlaceTurn.java
+++ src/main/java/frc/team3128/commands/CmdInPlaceTurn.java
@@ -6,21 +6,23 @@ import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.team3128.Constants.DriveConstants;
 import frc.team3128.subsystems.NAR_Drivetrain;
 
-public class CmdInPlace180Turn extends PIDCommand {
+public class CmdInPlaceTurn extends PIDCommand {
 
     private NAR_Drivetrain drivetrain;
+    private double turnDeg;
 
-    public CmdInPlace180Turn(NAR_Drivetrain drivetrain) {
+    public CmdInPlaceTurn(NAR_Drivetrain drivetrain, double turnDeg) {
 
         super(
             new PIDController(DriveConstants.TURN_kP, DriveConstants.TURN_kI, DriveConstants.TURN_kD),
             drivetrain::getHeading,
-            MathUtil.inputModulus(drivetrain.getHeading() + 180, -180, 180),
+            MathUtil.inputModulus(drivetrain.getHeading() + turnDeg, -180, 180),
             output -> drivetrain.tankDrive(output + Math.copySign(DriveConstants.TURN_kF, output), -output - Math.copySign(DriveConstants.TURN_kF, output)),
             drivetrain
         );
 
         this.drivetrain = drivetrain;
+        this.turnDeg = turnDeg;
 
         getController().enableContinuousInput(-180, 180);
         getController().setTolerance(DriveConstants.TURN_TOLERANCE);
@@ -31,7 +33,7 @@ public class CmdInPlace180Turn extends PIDCommand {
         super.initialize();
 
         // Hack to make sure the robot turns 180 degrees from current heading and not 180 degrees from 0
-        double setpoint = MathUtil.inputModulus(drivetrain.getHeading() + 180, -180, 180);
+        double setpoint = MathUtil.inputModulus(drivetrain.getHeading() + turnDeg, -180, 180);
         m_setpoint = () ->  setpoint;
     }
 
