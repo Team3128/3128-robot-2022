a484b19db182a7ac53d23a268321d54a95b482e2
Arav Chadha <aravchadha3894@gmail.com>
2022-10-25 18:52:30 -0700

    Added POVButtons to Xbox
    POVButtons not changed to triggers yet, will do in another pull request

--- src/main/java/frc/team3128/common/hardware/input/NAR_XboxController.java
+++ src/main/java/frc/team3128/common/hardware/input/NAR_XboxController.java
@@ -3,6 +3,7 @@ package frc.team3128.common.hardware.input;
 import java.util.HashMap;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class NAR_XboxController extends XboxController {
@@ -21,14 +22,18 @@ public class NAR_XboxController extends XboxController {
     };
     
     private HashMap<String, Trigger> buttons;
+    private POVButton[] povButtons;
 
     public NAR_XboxController(int port) {
         super(port);
         buttons = new HashMap<String, Trigger>();
+        povButtons = new POVButton[8];
         for (int i = 0; i < 10; i++) {
             int n = i + 1;
             buttons.put(buttonNames[i], new Trigger (() -> this.getRawButton(n)));
         }   
+        for (int i = 0; i < 8; i++)
+            povButtons[i] = new POVButton(this, i * 45);
     }
 
     public Trigger getButton(String buttonName) {
@@ -41,6 +46,26 @@ public class NAR_XboxController extends XboxController {
 
     public Trigger getRightTrigger() {
         return new Trigger (() -> getRightTriggerAxis() >= 0.5);
+    }
+
+    public POVButton getPOVButton(int i) {
+        return povButtons[i];
+    }
+
+    public POVButton getUpPOVButton() {
+        return getPOVButton(0);
+    }
+
+    public POVButton getDownPOVButton() {
+        return getPOVButton(4);
+    }
+
+    public POVButton getLeftPOVButton() {
+        return getPOVButton(6);
+    }
+
+    public POVButton getRightPOVButton() {
+        return getPOVButton(2);
     }
     
 }
