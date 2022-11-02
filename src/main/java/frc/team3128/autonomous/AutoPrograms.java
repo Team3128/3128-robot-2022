706f6e4ae22738382cfed5169f2cbc7631395b77
mika-okamoto <mika.disneypins@gmail.com>
2022-11-01 21:12:36 -0700

    workshop 11/1

--- src/main/java/frc/team3128/autonomous/AutoPrograms.java
+++ src/main/java/frc/team3128/autonomous/AutoPrograms.java
@@ -58,7 +58,7 @@ public class AutoPrograms {
     }
 
     private void initAutoSelector() {
-        String[] autoStrings = new String[] {"Intake 0", "Intake 1", "Intake 2", "4 Ball", "5 Ball", "S2H1", "Intake 1 Hoard 2", "S1H1", "S1I1", "S1H2", "Billiards"};
+        String[] autoStrings = new String[] {"Intake 0", "Intake 1", "Intake 2", "Intake 1 Hoard 2"};
         NarwhalDashboard.addAutos(autoStrings);
     }
 
@@ -113,76 +113,76 @@ public class AutoPrograms {
 
                                 new CmdShootTurnVision(-170));
                 break;
-            case "4 Ball":
-                initialPose = inverseRotation(get("4Ball_Terminal180_i").getInitialPose());
-                autoCommand = new SequentialCommandGroup(
-                                new CmdShootAlign().withTimeout(2),
-                                new CmdInPlaceTurn(180),
-                                //drive and intake 1 ball
-                                IntakePathCmd("4Ball_Terminal180_i"),  
+            // case "4 Ball":
+            //     initialPose = inverseRotation(get("4Ball_Terminal180_i").getInitialPose());
+            //     autoCommand = new SequentialCommandGroup(
+            //                     new CmdShootAlign().withTimeout(2),
+            //                     new CmdInPlaceTurn(180),
+            //                     //drive and intake 1 ball
+            //                     IntakePathCmd("4Ball_Terminal180_i"),  
 
-                                //turn and shoot 2 balls
-                                new CmdInPlaceTurn(180),
-                                new CmdShoot(),
+            //                     //turn and shoot 2 balls
+            //                     new CmdInPlaceTurn(180),
+            //                     new CmdShoot(),
 
-                                //drive to ball and terminal and intake
-                                IntakePathCmd("4Ball_Terminal180_ii"), 
+            //                     //drive to ball and terminal and intake
+            //                     IntakePathCmd("4Ball_Terminal180_ii"), 
 
-                                new CmdExtendIntakeAndRun().withTimeout(1), // is this still needed?
+            //                     new CmdExtendIntakeAndRun().withTimeout(1), // is this still needed?
 
-                                //drive to tarmac and shoot
-                                trajectoryCmd("Terminal2Tarmac"),
-                                new CmdInPlaceTurn(180),
-                                new CmdShootAlign().withTimeout(2));
-                break;
-            case "5 Ball":
-                initialPose = inverseRotation(get("3Ballv2_i").getInitialPose());
-                autoCommand = new SequentialCommandGroup(
-                                new CmdShootAlign().withTimeout(2.5),
-                                new CmdInPlaceTurn(180),
+            //                     //drive to tarmac and shoot
+            //                     trajectoryCmd("Terminal2Tarmac"),
+            //                     new CmdInPlaceTurn(180),
+            //                     new CmdShootAlign().withTimeout(2));
+            //     break;
+            // case "5 Ball":
+            //     initialPose = inverseRotation(get("3Ballv2_i").getInitialPose());
+            //     autoCommand = new SequentialCommandGroup(
+            //                     new CmdShootAlign().withTimeout(2.5),
+            //                     new CmdInPlaceTurn(180),
 
-                                IntakePathCmd("3Ballv2_i"), 
+            //                     IntakePathCmd("3Ballv2_i"), 
 
-                                new CmdInPlaceTurn(180),
-                                new CmdShootAlign().withTimeout(2),
+            //                     new CmdInPlaceTurn(180),
+            //                     new CmdShootAlign().withTimeout(2),
 
-                                IntakePathCmd("3Ballv2_ii"),
+            //                     IntakePathCmd("3Ballv2_ii"),
 
-                                new CmdShootTurnVision(-170),
+            //                     new CmdShootTurnVision(-170),
 
-                                IntakePathCmd("5Ballv2_i"),
+            //                     IntakePathCmd("5Ballv2_i"),
                                 
-                                new InstantCommand(() -> drive.stop()),
+            //                     new InstantCommand(() -> drive.stop()),
 
-                                trajectoryCmd("5Ballv2_ii"),
+            //                     trajectoryCmd("5Ballv2_ii"),
 
-                                new CmdShootTurnVision(-170)
-                            );
-                break;
-            case "S2H1":
-                initialPose = get("S2H2_i").getInitialPose();
-                autoCommand = new SequentialCommandGroup(
-                                //drive and intake ball
+            //                     new CmdShootTurnVision(-170)
+            //                 );
+            //     break;
+            // case "S2H1":
+            //     initialPose = get("S2H2_i").getInitialPose();
+            //     autoCommand = new SequentialCommandGroup(
+            //                     //drive and intake ball
 
-                                IntakePathCmd("S2H2_i"),
+            //                     IntakePathCmd("S2H2_i"),
 
-                                //turn and shoot
-                                new CmdInPlaceTurn(180),
-                                new CmdShootAlign().withTimeout(2),
+            //                     //turn and shoot
+            //                     new CmdInPlaceTurn(180),
+            //                     new CmdShootAlign().withTimeout(2),
 
-                                //turn and hoard first ball
-                                new CmdInPlaceTurn(90),
+            //                     //turn and hoard first ball
+            //                     new CmdInPlaceTurn(90),
 
-                                IntakePathCmd("S2H2_ii"),
+            //                     IntakePathCmd("S2H2_ii"),
 
-                                //drive behind hub
-                                new CmdInPlaceTurn(-90),
-                                trajectoryCmd("S2H1"),
+            //                     //drive behind hub
+            //                     new CmdInPlaceTurn(-90),
+            //                     trajectoryCmd("S2H1"),
 
-                                //outtake balls behind hub
-                                new CmdExtendIntake(),
-                                new CmdOuttake(0.5).withTimeout(2));
-                break;
+            //                     //outtake balls behind hub
+            //                     new CmdExtendIntake(),
+            //                     new CmdOuttake(0.5).withTimeout(2));
+            //     break;
             case "Intake 1 Hoard 2":
                 initialPose = inverseRotation(get("S2H2_i").getInitialPose());
                 autoCommand = new SequentialCommandGroup(
@@ -216,89 +216,89 @@ public class AutoPrograms {
                                 new CmdExtendIntake(),
                                 new CmdOuttake(0.4).withTimeout(1));
                 break;
-            case "S1H1":
-                initialPose = get("S1H1_i").getInitialPose();
-                autoCommand = new SequentialCommandGroup(          
-                                //drive and intake enemy ball          
-                                IntakePathCmd("S1H1_i"), 
-                                new InstantCommand(() -> drive.stop()),
+            // case "S1H1":
+            //     initialPose = get("S1H1_i").getInitialPose();
+            //     autoCommand = new SequentialCommandGroup(          
+            //                     //drive and intake enemy ball          
+            //                     IntakePathCmd("S1H1_i"), 
+            //                     new InstantCommand(() -> drive.stop()),
                                 
-                                //turn and shoot 1 ball
-                                new CmdInPlaceTurn(180),
+            //                     //turn and shoot 1 ball
+            //                     new CmdInPlaceTurn(180),
                                 
-                                new CmdShootAlignSingle().withTimeout(2),
+            //                     new CmdShootAlignSingle().withTimeout(2),
                                 
-                                //hide enemy ball behind hub
-                                trajectoryCmd("S1H1_ii"),
-                                new CmdExtendIntake(),
-                                new CmdOuttake(0.5).withTimeout(1));
-                break;
-            case "S1H2":
-                initialPose = inverseRotation(get("S1H1_i").getInitialPose());
-                autoCommand = new SequentialCommandGroup(
+            //                     //hide enemy ball behind hub
+            //                     trajectoryCmd("S1H1_ii"),
+            //                     new CmdExtendIntake(),
+            //                     new CmdOuttake(0.5).withTimeout(1));
+            //     break;
+            // case "S1H2":
+            //     initialPose = inverseRotation(get("S1H1_i").getInitialPose());
+            //     autoCommand = new SequentialCommandGroup(
 
-                                new CmdShootAlign().withTimeout(3),
+            //                     new CmdShootAlign().withTimeout(3),
 
-                                new CmdInPlaceTurn(180),
-                                //drive and intake enemy ball
-                                IntakePathCmd("S1H1_i"), 
-                                new InstantCommand(() -> drive.stop()),
+            //                     new CmdInPlaceTurn(180),
+            //                     //drive and intake enemy ball
+            //                     IntakePathCmd("S1H1_i"), 
+            //                     new InstantCommand(() -> drive.stop()),
 
-                                //turn and shoot 1 ball
-                                new CmdInPlaceTurn(180),
+            //                     //turn and shoot 1 ball
+            //                     new CmdInPlaceTurn(180),
 
-                                //drive and intake enemy ball
-                                IntakePathCmd("S1H2_ii"), 
+            //                     //drive and intake enemy ball
+            //                     IntakePathCmd("S1H2_ii"), 
 
-                                //hide enemy balls behind hub
-                                new CmdInPlaceTurn(180),
+            //                     //hide enemy balls behind hub
+            //                     new CmdInPlaceTurn(180),
                     
-                                trajectoryCmd("S1H2_iii"), 
+            //                     trajectoryCmd("S1H2_iii"), 
                                 
-                                new CmdExtendIntake(),
-                                new CmdOuttake(0.5).withTimeout(2));
-                break;
-            case "S1I1":
-                initialPose = get("S1H1_i").getInitialPose();
-                autoCommand = new SequentialCommandGroup(
-                                //drive and intake enemy ball
-                                IntakePathCmd("S1H1_i"), 
-                                new InstantCommand(() -> drive.stop()),
+            //                     new CmdExtendIntake(),
+            //                     new CmdOuttake(0.5).withTimeout(2));
+            //     break;
+            // case "S1I1":
+            //     initialPose = get("S1H1_i").getInitialPose();
+            //     autoCommand = new SequentialCommandGroup(
+            //                     //drive and intake enemy ball
+            //                     IntakePathCmd("S1H1_i"), 
+            //                     new InstantCommand(() -> drive.stop()),
 
-                                //turn and shoot 1 ball
-                                new CmdInPlaceTurn(180),
+            //                     //turn and shoot 1 ball
+            //                     new CmdInPlaceTurn(180),
                                 
-                                new CmdShootAlignSingle().withTimeout(2));
-                break;
-            case "Billiards":
-                // initial position: (6.8, 6.272, 45 deg - should be approx. pointing straight at the ball to knock)
-                initialPose = new Pose2d(6.8, 6.272, Rotation2d.fromDegrees(45));
-                autoCommand = new SequentialCommandGroup (
-                                // outtake ball
-                                new SequentialCommandGroup(
-                                    new CmdExtendIntake(),
-                                    new CmdOuttake(0.85).withTimeout(1.5)
-                                ).withTimeout(2),
+            //                     new CmdShootAlignSingle().withTimeout(2));
+            //     break;
+            // case "Billiards":
+            //     // initial position: (6.8, 6.272, 45 deg - should be approx. pointing straight at the ball to knock)
+            //     initialPose = new Pose2d(6.8, 6.272, Rotation2d.fromDegrees(45));
+            //     autoCommand = new SequentialCommandGroup (
+            //                     // outtake ball
+            //                     new SequentialCommandGroup(
+            //                         new CmdExtendIntake(),
+            //                         new CmdOuttake(0.85).withTimeout(1.5)
+            //                     ).withTimeout(2),
 
-                                //turn, drive, and intake enemy ball
-                                new CmdInPlaceTurn(70),
+            //                     //turn, drive, and intake enemy ball
+            //                     new CmdInPlaceTurn(70),
 
-                                IntakePathCmd("Billiards_i"),
+            //                     IntakePathCmd("Billiards_i"),
 
-                                //turn and eject enemy ball
-                                new CmdInPlaceTurn(55),
+            //                     //turn and eject enemy ball
+            //                     new CmdInPlaceTurn(55),
 
-                                new CmdExtendIntake(),
-                                new CmdOuttake(0.6).withTimeout(1.5),
+            //                     new CmdExtendIntake(),
+            //                     new CmdOuttake(0.6).withTimeout(1.5),
 
-                                //drive and intake ball
-                                IntakePathCmd("Billiards_ii"),
+            //                     //drive and intake ball
+            //                     IntakePathCmd("Billiards_ii"),
 
-                                //turn and shoot
-                                new CmdInPlaceTurn(96),
+            //                     //turn and shoot
+            //                     new CmdInPlaceTurn(96),
 
-                                new CmdShootAlign().withTimeout(2));
-                break;
+            //                     new CmdShootAlign().withTimeout(2));
+            //     break;
             default: 
                 Log.info("Auto Selector", "Something went wrong in getting the auto name - misspelling?");
                 break;
